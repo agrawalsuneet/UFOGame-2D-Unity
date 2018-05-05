419b4552cd1c62a6579bc9be474e72e87b2f3a2e
Suneet Agrawal <agrawalsuneet@gmail.com>
2018-05-05 20:22:35 +0100

    added pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,4 +21,14 @@ public class PlayerController : MonoBehaviour {
 
 		playerRB2D.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+
 }
